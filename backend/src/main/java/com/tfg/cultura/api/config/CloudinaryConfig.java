f1750db68eb3e0ad082b107ce4d35784e8467e2f
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2026-04-17 17:56:36 +0000

    fix(workflows): :bug: fix backend tests workflow
    - Cloudinary will no longer be configured in the testing context.
    
    Refs #3

--- backend/src/main/java/com/tfg/cultura/api/config/CloudinaryConfig.java
+++ backend/src/main/java/com/tfg/cultura/api/config/CloudinaryConfig.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.cloudinary.Cloudinary;
 
 @Configuration
+@ConditionalOnProperty(name = "cloudinary.enabled", havingValue = "true", matchIfMissing = true)
 public class CloudinaryConfig {
 
     @Value("${cloudinary.cloud_name}")

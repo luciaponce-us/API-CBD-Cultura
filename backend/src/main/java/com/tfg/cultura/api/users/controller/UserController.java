8c2c711672523732ba4a5e0710f611aeab3bed39
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2026-04-23 14:04:01 +0000

    style(backend): :art: format UserController
    Refs #7

--- backend/src/main/java/com/tfg/cultura/api/users/controller/UserController.java
+++ backend/src/main/java/com/tfg/cultura/api/users/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.tfg.cultura.api.users.controller;
 
+import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,63 +12,62 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.tfg.cultura.api.users.model.dto.UserResponse;
-import com.tfg.cultura.api.users.model.dto.UserLoginRequest;
-import com.tfg.cultura.api.users.model.dto.UserRegisterRequest;
-import com.tfg.cultura.api.users.service.UserService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
+
+import com.tfg.cultura.api.users.model.dto.UserLoginRequest;
+import com.tfg.cultura.api.users.model.dto.UserRegisterRequest;
+import com.tfg.cultura.api.users.model.dto.UserResponse;
+import com.tfg.cultura.api.users.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 @Tag(name = "Users", description = "Módulo de gestión de usuarios")
 public class UserController {
 
-    private final UserService userService;
+        private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+        public UserController(UserService userService) {
+                this.userService = userService;
+        }
 
-    @Operation(summary = "RF-01: Registrar usuarios", description = "Como usuario, quiero poder solicitar mi registro en el sistema, para poder iniciar sesión.")
-    @PostMapping(value = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Registro correcto"),
-            @ApiResponse(responseCode = "409", description = "RN-01: DNI único por usuario / RN-02: Apodo único por usuario"),
-            @ApiResponse(responseCode = "400", description = "Los datos introducidos no son válidos")
-    })
-    public ResponseEntity<UserResponse> register(
-            @Valid @Parameter(description = "Datos del usuario en JSON", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) @RequestPart("user") UserRegisterRequest request,
-            @RequestPart(value = "avatar", required = false) MultipartFile avatar) {
-        request.setAvatar(avatar);
-        UserResponse user = userService.register(request);
+        @Operation(summary = "RF-01: Registrar usuarios", description = "Como usuario, quiero poder solicitar mi registro en el sistema, para poder iniciar sesión.")
+        @PostMapping(value = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "201", description = "Registro correcto"),
+                        @ApiResponse(responseCode = "409", description = "RN-01: DNI único por usuario / RN-02: Apodo único por usuario"),
+                        @ApiResponse(responseCode = "400", description = "Los datos introducidos no son válidos")
+        })
+        public ResponseEntity<UserResponse> register(
+                        @Valid @Parameter(description = "Datos del usuario en JSON", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) @RequestPart("user") UserRegisterRequest request,
+                        @RequestPart(value = "avatar", required = false) MultipartFile avatar) {
+                request.setAvatar(avatar);
+                UserResponse user = userService.register(request);
 
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(user);
+                return ResponseEntity
+                                .status(HttpStatus.CREATED)
+                                .body(user);
 
-    }
+        }
 
-    @Operation(summary = "RF-02: Iniciar sesión", description = "Como usuario registrado, quiero poder iniciar sesión usando las credenciales con las que me registré previamente, para poder acceder a las funciones del sistema que requieran de autenticación")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Login correcto"),
-            @ApiResponse(responseCode = "401", description = "RN-04: Inicio de sesión - Unauthorized - Credenciales inválidas"),
-            @ApiResponse(responseCode = "403", description = "RN-04: Inicio de sesión - User Disabled"),
-            @ApiResponse(responseCode = "404", description = "RN-04: Inicio de sesión - User Not Found"),
-    })
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody UserLoginRequest request) {
-        String token = userService.login(request);
+        @Operation(summary = "RF-02: Iniciar sesión", description = "Como usuario registrado, quiero poder iniciar sesión usando las credenciales con las que me registré previamente, para poder acceder a las funciones del sistema que requieran de autenticación")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Login correcto"),
+                        @ApiResponse(responseCode = "401", description = "RN-04: Inicio de sesión - Unauthorized - Credenciales inválidas"),
+                        @ApiResponse(responseCode = "403", description = "RN-04: Inicio de sesión - User Disabled"),
+                        @ApiResponse(responseCode = "404", description = "RN-04: Inicio de sesión - User Not Found"),
+        })
+        @PostMapping("/login")
+        public ResponseEntity<String> login(@Valid @RequestBody UserLoginRequest request) {
+                String token = userService.login(request);
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(token);
-    }
+                return ResponseEntity
+                                .status(HttpStatus.OK)
+                                .body(token);
+        }
 
 }

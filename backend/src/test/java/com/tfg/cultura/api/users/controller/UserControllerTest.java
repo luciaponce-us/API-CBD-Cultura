21a6598d35c0f385ed84c204b4b1953ae684baaa
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2026-04-23 14:50:34 +0000

    test(backend): :white_check_mark: add user login tests at UserControllerTest
    Refs #7

--- backend/src/test/java/com/tfg/cultura/api/users/controller/UserControllerTest.java
+++ backend/src/test/java/com/tfg/cultura/api/users/controller/UserControllerTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.Mockito;
-
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -15,8 +17,10 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import com.tfg.cultura.api.config.exception.ApiErrorBuilder;
 import com.tfg.cultura.api.config.exception.GlobalExceptionHandler;
 import com.tfg.cultura.api.users.exception.UserAlreadyExistsException;
+import com.tfg.cultura.api.users.exception.UserNotFoundException;
 import com.tfg.cultura.api.users.exception.UsersExceptionHandler;
 import com.tfg.cultura.api.users.model.User;
+import com.tfg.cultura.api.users.model.dto.UserLoginRequest;
 import com.tfg.cultura.api.users.model.dto.UserRegisterRequest;
 import com.tfg.cultura.api.users.model.dto.UserResponse;
 import com.tfg.cultura.api.users.service.UserService;
@@ -25,6 +29,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class UserControllerTest {
@@ -35,10 +40,13 @@ class UserControllerTest {
 
         private static final String BASE_URL = "/api/users";
         private static final String REGISTER_URL = BASE_URL + "/register";
+        private static final String LOGIN_URL = BASE_URL + "/login";
 
-        private UserRegisterRequest userRegisterRequest;
+        private UserRegisterRequest registerRequest;
+        private UserLoginRequest loginRequest;
         private UserResponse userResponse;
-        ApiErrorBuilder apiErrorBuilder = new ApiErrorBuilder();
+        private ApiErrorBuilder apiErrorBuilder = new ApiErrorBuilder();
+        private final ObjectMapper objectMapper = new ObjectMapper();
 
         @BeforeEach
         void setup() {
@@ -57,7 +65,7 @@ class UserControllerTest {
                                                 new UsersExceptionHandler(apiErrorBuilder))
                                 .build();
 
-                userRegisterRequest = UserRegisterRequest.builder()
+                registerRequest = UserRegisterRequest.builder()
                                 .username("test")
                                 .password("12345678")
                                 .name("John")
@@ -78,66 +86,113 @@ class UserControllerTest {
                                 .build();
 
                 userResponse = new UserResponse(createdUser);
+
+                loginRequest = UserLoginRequest.builder()
+                                .username("test")
+                                .password("12345678")
+                                .build();
+
         }
 
         @Test
         void register_success() throws Exception {
-                ObjectMapper objectMapper = new ObjectMapper();
-
                 when(userService.register(any())).thenReturn(userResponse);
 
-                String userJson = objectMapper.writeValueAsString(userRegisterRequest);
-
-                MockMultipartFile userPart = new MockMultipartFile(
-                                "user",
-                                "",
-                                "application/json",
-                                userJson.getBytes());
-
                 mockMvc.perform(multipart(REGISTER_URL)
-                                .file(userPart))
+                                .file(userPart(registerRequest)))
                                 .andExpect(status().isCreated())
-                                .andExpect(jsonPath("$.username").value(userRegisterRequest.getUsername()));
+                                .andExpect(jsonPath("$.username").value(registerRequest.getUsername()));
         }
 
         @Test
         void register_fail_user_already_exists() throws Exception {
-                ObjectMapper objectMapper = new ObjectMapper();
                 UserAlreadyExistsException ex = new UserAlreadyExistsException("El nombre de usuario ya existe");
 
                 when(userService.register(any())).thenThrow(ex);
 
-                String userJson = objectMapper.writeValueAsString(userRegisterRequest);
-
-                MockMultipartFile userPart = new MockMultipartFile(
-                                "user",
-                                "",
-                                "application/json",
-                                userJson.getBytes());
-
                 mockMvc.perform(multipart(REGISTER_URL)
-                                .file(userPart))
+                                .file(userPart(registerRequest)))
                                 .andExpect(status().isConflict())
-                                .andExpect(jsonPath("$.message").value(ex.getMessage()));
+                                .andExpect(jsonPath("$.message").exists());
         }
 
         @Test
         void register_fail_invalid_data() throws Exception {
-                ObjectMapper objectMapper = new ObjectMapper();
+                registerRequest.setEmail("invalid-email");
 
-                userRegisterRequest.setEmail("invalid-email");
+                mockMvc.perform(multipart(REGISTER_URL)
+                                .file(userPart(registerRequest)))
+                                .andExpect(status().isBadRequest())
+                                .andExpect(jsonPath("$.message").exists());
+        }
 
-                String userJson = objectMapper.writeValueAsString(userRegisterRequest);
+        // ====== LOGIN ========
 
-                MockMultipartFile userPart = new MockMultipartFile(
+        @Test
+        void login_success() throws Exception {
+                String token = "token123";
+                when(userService.login(any())).thenReturn(token);
+
+                mockMvc.perform(post(LOGIN_URL)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toJson(loginRequest)))
+                                .andExpect(status().isOk())
+                                .andExpect(content().string(token));
+
+        }
+
+        @Test
+        void login_fail_invalid_credentials() throws Exception {
+                BadCredentialsException ex = new BadCredentialsException("Credenciales inválidas");
+                when(userService.login(any())).thenThrow(ex);
+
+                mockMvc.perform(post(LOGIN_URL)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toJson(loginRequest)))
+                                .andExpect(status().isUnauthorized())
+                                .andExpect(jsonPath("$.message").value(ex.getMessage()));
+
+        }
+
+        @Test
+        void login_fail_user_not_found() throws Exception {
+                String username = loginRequest.getUsername();
+                String message = "El usuario con username " + username + " no existe";
+                UserNotFoundException ex = new UserNotFoundException(message);
+                when(userService.login(any())).thenThrow(ex);
+
+                mockMvc.perform(post(LOGIN_URL)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toJson(loginRequest)))
+                                .andExpect(status().isNotFound())
+                                .andExpect(jsonPath("$.message").value(message));
+        }
+
+        @Test
+        void login_fail_user_disabled() throws Exception {
+                String message = "El usuario está desactivado";
+                DisabledException ex = new DisabledException(message);
+                when(userService.login(any())).thenThrow(ex);
+
+                mockMvc.perform(post(LOGIN_URL)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toJson(loginRequest)))
+                                .andExpect(status().isForbidden())
+                                .andExpect(jsonPath("$.message").value(message));
+        }
+
+        // Helpers
+
+        private String toJson(Object obj) throws Exception {
+                return objectMapper.writeValueAsString(obj);
+        }
+
+        private MockMultipartFile userPart(Object obj) throws Exception {
+                return new MockMultipartFile(
                                 "user",
                                 "",
                                 "application/json",
-                                userJson.getBytes());
-
-                mockMvc.perform(multipart(REGISTER_URL)
-                                .file(userPart))
-                                .andExpect(status().isBadRequest())
-                                .andExpect(jsonPath("$.message").value("Campo email: El email no es válido"));
+                                toJson(obj).getBytes());
         }
+
 }

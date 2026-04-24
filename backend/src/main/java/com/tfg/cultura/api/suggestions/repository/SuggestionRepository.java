5c14a6583d25423d831034dc71eee6ddf2899fa9
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2026-04-24 17:22:49 +0000

    feat(backend): :sparkles: add suggestion repository
    Refs #13

---
+++ backend/src/main/java/com/tfg/cultura/api/suggestions/repository/SuggestionRepository.java
@@ -0,0 +1,9 @@
+package com.tfg.cultura.api.suggestions.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.tfg.cultura.api.suggestions.model.Suggestion;
+
+public interface SuggestionRepository extends MongoRepository<Suggestion,String> {
+    
+}
